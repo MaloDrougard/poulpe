7c7a4a3853149911f1d3c987958d3ae7172a3495
Malo <malo.drougard@protonmail.com>
2025-02-16 12:49:37 +0100

    ui

--- Api.cpp
+++ Api.cpp
@@ -20,6 +20,26 @@ void handleRoot(EthernetClient& client) {
             client.println("Connection: close");
             client.println();
             client.println("{\"status\":\"ok\"}");
+        } else if (request.indexOf("GET /setpompe") >= 0) {
+            int pIndex = request.indexOf("p=");
+            int tIndex = request.indexOf("t=");
+            if (pIndex >= 0 && tIndex >= 0) {
+                int pValue = request.substring(pIndex + 2, request.indexOf('&', pIndex)).toInt();
+                int tValue = request.substring(tIndex + 2, request.indexOf(' ', tIndex)).toInt();
+                // Handle the setpompe logic here with pValue and tValue
+                client.println("HTTP/1.1 200 OK");
+                client.println("Content-Type: application/json");
+                client.println("Connection: close");
+                client.println("Access-Control-Allow-Origin: *");
+                client.println();
+                client.println("{\"status\":\"ok\",\"p\":" + String(pValue) + ",\"t\":" + String(tValue) + "}");
+            } else {
+                client.println("HTTP/1.1 400 Bad Request");
+                client.println("Content-Type: text/plain");
+                client.println("Connection: close");
+                client.println();
+                client.println("400 Bad Request");
+            }
         } else {
             client.println("HTTP/1.1 404 Not Found");
             client.println("Content-Type: text/plain");
